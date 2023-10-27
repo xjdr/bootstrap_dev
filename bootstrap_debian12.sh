6acbf7839070fdb86fd2df137ca92fc1a9c7c347
Jeff Rose <xjdr@ironwood.ai>
2023-10-26 17:31:51 -0700

    now tested and approved

--- bootstrap_debian12.sh
+++ bootstrap_debian12.sh
@@ -18,9 +18,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo groupadd docker
+#sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
+#newgrp docker
 
 docker run hello-world
 
@@ -79,3 +79,4 @@ EOF
 
 # ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh
